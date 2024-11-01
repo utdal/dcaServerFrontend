3331c9376c5c75564649f6fe1c3a2e10a4d4fdfe
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2024-11-01 02:52:36 -0500

    Added conditional checking for auth ID support
    Based on whether or not it is a CIF file that is being supplied. The user now can use a checkbox to specify the type of chain they're supplying. If they're supplying a PDB file, they don't get that option.

--- src/components/PDBSettings.js
+++ src/components/PDBSettings.js
@@ -4,15 +4,78 @@ import FormGroup from '@mui/material/FormGroup';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
-const PDBSettings = ({ distThresh, handleDistThreshChange, caOnly, handleCaOnlyChange }) => {
+const PDBSettings = ({ distThresh, handleDistThreshChange, caOnly, handleCaOnlyChange, chain1, handleChain1Change, chain2, handleChain2Change, selectedPDBTypes }) => {
   return (
-    <Box sx={{}}>
+    <Box>
       <p>Structural Contact Distance Threshold:</p>
-        <TextField
-            label = "Threshold (Å)"
-            value={distThresh}
-            onChange={handleDistThreshChange}>
-        </TextField>
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 25, flexDirection: 'row'}}>
+          <TextField
+              label = "Threshold (Å)"
+              value={distThresh}
+              onChange={handleDistThreshChange}>
+          </TextField>
+
+          {selectedPDBTypes.CIF === true ? 
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 5, flexDirection: 'row'}}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+                    <TextField
+                      label="Chain 1 ID"
+                      name="chainId"
+                      value={chain1.chainId}
+                      onChange={handleChain1Change}
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          name="isAuth"
+                          checked={chain1.isAuth}
+                          onChange={handleChain1Change}
+                        />
+                      }
+                      label="Auth Chain"
+                    />
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+                <TextField
+                  label="Chain 2 ID"
+                  name="chainId"
+                  value={chain2.chainId}
+                  onChange={handleChain2Change}
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="isAuth"
+                      checked={chain2.isAuth}
+                      onChange={handleChain2Change}
+                    />
+                  }
+                  label="Auth Chain"
+                />
+              </Box>
+            </Box>
+          :
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 5, flexDirection: 'row'}}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+                <TextField
+                  label="Chain 1 ID"
+                  name="chainId"
+                  value={chain1.chainId}
+                  onChange={handleChain1Change}
+                />
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+                <TextField
+                  label="Chain 2 ID"
+                  name="chainId"
+                  value={chain2.chainId}
+                  onChange={handleChain2Change}
+                />
+              </Box>
+            </Box>
+          }
+          
+        </Box>
         <FormGroup sx={{ alignItems: 'center' }}>
             <FormControlLabel control={<Checkbox checked={caOnly} onChange={handleCaOnlyChange}/>} label="Alpha-carbon contacts only" />
         </FormGroup>
@@ -21,3 +84,10 @@ const PDBSettings = ({ distThresh, handleDistThreshChange, caOnly, handleCaOnlyC
 };
 
 export default PDBSettings;
+
+
+
+
+
+
+
